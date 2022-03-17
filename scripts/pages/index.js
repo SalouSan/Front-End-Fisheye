b59ad6b513704d94bfb7f507882de86bd0c51e76
Salou Sankhare <Salousankhare@gmail.com>
2022-03-17 11:53:37 +0100

    ajout fonctions

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,37 +1,84 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
+
+async function getPhotographers() {
+    try {
+    let response = await fetch ('/data/photographers.json')
+    let data = await response.json()
+     console.log(data);
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+    const photographers = [
             {
-                "name": "Ma data test",
-                "id": 1,
+                "name": "Mimi Keel",
+                "id": 243,
+                "city": "London",
+                "country": "UK",
+                "tagline": "Voir le beau dans le quotidien",
+                "price": 400,
+                "portrait": "MimiKeel.jpg"
+            },
+            {
+                "name": "Ellie-Rose Wilkens",
+                "id": 930,
                 "city": "Paris",
                 "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
+                "tagline": "Capturer des compositions complexes",
+                "price": 250,
+                "portrait": "EllieRoseWilkens.jpg"
             },
             {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
+                "name": "Tracy Galindo",
+                "id": 82,
+                "city": "Montreal",
+                "country": "Canada",
+                "tagline": "Photographe freelance",
                 "price": 500,
-                "portrait": "account.png"
+                "portrait": "TracyGalindo.jpg"
             },
+            {
+                "name": "Nabeel Bradford",
+                "id": 527,
+                "city": "Mexico City",
+                "country": "Mexico",
+                "tagline": "Toujours aller de l'avant",
+                "price": 350,
+                "portrait": "NabeelBradford.jpg"
+            },
+            {
+                "name": "Rhode Dubois",
+                "id": 925,
+                "city": "Barcelona",
+                "country": "Spain",
+                "tagline": "Je crée des souvenirs",
+                "price": 275,
+                "portrait": "RhodeDubois.jpg"
+            },
+            {
+                "name": "Marcel Nikolic",
+                "id": 195,
+                "city": "Berlin",
+                "country": "Germany",
+                "tagline": "Toujours à la recherche de LA photo",
+                "price": 300,
+                "portrait": "MarcelNikolic.jpg"
+            }
+
         ]
         // et bien retourner le tableau photographers seulement une fois
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: [...photographers]})
     }
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
+        photographers.forEach((photographers) => {
             const photographerModel = photographerFactory(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
+            
         });
     };
 
@@ -42,4 +89,3 @@
     };
     
     init();
-    
