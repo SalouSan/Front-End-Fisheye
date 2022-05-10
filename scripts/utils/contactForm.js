b5fb43680cf74a2107afb664a5066f410ab000d6
Salou Sankhare <Salousankhare@gmail.com>
2022-05-10 10:43:00 +0200

    creation modale

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -20,31 +20,59 @@ let formData = document.querySelector(".formData");
 
 function firstValidate () {
     let firstNameValue = firstName.value.trim();
-    if (firstNameValue.length === 2) {
-        console.log("Vous devez entrer 2 caracteres ou plus");
+    if (firstNameValue.length < 2) {
+        return false
+    }
+    else {
+        return true
     }
 }
 function nameValidate () {
     let lastNameValue = lastName.value.trim();
-    if (lastNameValue.length === 2) {
-        console.log("Vous devez entrer 2 caracteres ou plus");
+    if (lastNameValue.length < 2) {
+        return false
+    }
+    else {
+        return true
     }
 }
 function emailValidate () {
     let emailValue = email.value.trim();
     let regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
     if (emailValue.match(regx)) {
-        console.log("Votre adresse mail est correcte");
+        return true
     }
     else {
-        console.log("Veuillez bien saisir votre adresse mail");
+        return false
     }
 }
 function messageValidate () {
     let messageValue = message.value.trim();
     if (messageValue.length <2) {
-        console.log("Veuillez entrer plus de 2 caractères");
+        return false
     }
+    else {
+        return true
+    }
+}
+// function message d'erreurs et de succès 
+
+function setError (input,message) {
+    let formData = input.parentElement;
+    let errorElement = document.querySelector("small");
+    errorElement.innerText= message;
+    errorElement.style.color= "red";
+    formData.classList.add("error");
+    formData.classList.remove("success");
+    
+}
+function setSucess (input) {
+    let formData = input.parentElement;
+    let errorElement = document.querySelector("small");
+    errorElement.style.visibility= "hidden";
+    formData.classList.add("success");
+    formData.classList.remove("error");
+
 }
 
 // form add event listener 
@@ -57,3 +85,46 @@ formData.addEventListener("input", function (e){
 
 });
 
+// function qui valide tous les inputs en meme temps 
+function globalValidate () {
+    if (firstValidate ()){
+        setSucess(firstName);
+    }
+    else {
+        setError(firstName, "Vous devez entrer au moins deux caractères pour le prénom");
+    }
+    if (nameValidate()){
+        setSucess(lastName);
+    }
+    else {
+        setError(lastName, "Vous devez entrer au moins deux caractères pour le nom");
+    }
+    if (emailValidate()){
+        setSucess(email);
+    }
+    else {
+        setError(email, "Merci d'entrer un email valide");
+    }
+    if (messageValidate()){
+        setSucess(message);
+    }
+    else {
+        setError(message, "Merci de bien entrer au moins caractères");
+    }
+    if (firstValidate() && nameValidate () && emailValidate () && messageValidate){
+
+    }
+}
+
+
+// add event listener submit 
+
+formData.addEventListener("submit", function (e){
+    e.preventDefault();
+    globalValidate();
+})
+// message succès
+
+function successMessage () {
+    
+}
