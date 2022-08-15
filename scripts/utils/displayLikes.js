4d64a40df7a1ff52eeeff129a3b03905897f03fb
Salou Sankhare <Salousankhare@gmail.com>
2022-08-15 19:07:41 +0200

    Refactorisation handleLikes

--- scripts/utils/displayLikes.js
+++ scripts/utils/displayLikes.js
@@ -1,20 +1,19 @@
 export function handleLikes() {
-
+	function likes(e){
+		let counter = e.target.closest(".content__items").querySelector(".content__items--like");
+		counter.innerHTML = parseInt(counter.innerHTML)+1;
+		let counterTotal = document.querySelector(".counter_content .counter");
+		counterTotal.innerHTML = parseInt(counterTotal.innerHTML)+1;	
+	}
 	let heart = document.querySelectorAll(".content__items--heart");
 	for (let i of heart){
-		i.addEventListener("click", function (e){
-			let counter = e.target.closest(".content__items").querySelector(".content__items--like");
-			counter.innerHTML = parseInt(counter.innerHTML)+1;
-			let counterTotal = document.querySelector(".counter_content .counter");
-			counterTotal.innerHTML = parseInt(counterTotal.innerHTML)+1;	
+		i.addEventListener("click", (e) =>{
+			likes(e);
 
 		});
 		i.addEventListener("keyup", (e) =>{
 			if (e.key == "Enter") {
-				let counter = e.target.closest(".content__items").querySelector(".content__items--like");
-				counter.innerHTML = parseInt(counter.innerHTML)+1;
-				let counterTotal = document.querySelector(".counter_content .counter");
-				counterTotal.innerHTML = parseInt(counterTotal.innerHTML)+1;	
+				likes(e);	
 			}
 		});
 	}
